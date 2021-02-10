aa2fb5f4eda84823979105473763a1f95509bcdb
Szymon Warszawa <szymonwarszawa@gmail.com>
2021-02-10 21:15:37 +0000

    accesing Process Object in Node.

---
+++ ex1.js
@@ -0,0 +1,21 @@
+//accesing the Proccess Object
+let initialMemory = process.memoryUsage().heapUsed;
+let word = process.argv[2];
+
+console.log(`Your word is ${word}`);
+
+// Create a new array
+let wordArray = [];
+
+// Loop 1000 times, pushing into the array each time
+for (let i = 0; i < 1000; i++) {
+  wordArray.push(`${word} count: ${i}`);
+}
+
+console.log(
+  `Starting memory usage: ${initialMemory}. \nCurrent memory usage: ${
+    process.memoryUsage().heapUsed
+  }. \nAfter using the loop to add elements to the array,\n the process is using ${
+    process.memoryUsage().heapUsed - initialMemory
+  } more bytes of memory.`
+);
